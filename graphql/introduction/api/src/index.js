959a7368ee49c2769d1c3949df352b528d196b52
zevdvlpr <zevdvlpr@gmail.com>
2020-11-18 11:46:42 -0300

    :bulb: chore: introduction

---
+++ graphql/introduction/api/src/index.js
@@ -0,0 +1,8 @@
+import 'dotenv/config';
+
+import startServer from './startServer';
+
+import typeDefs from './graphql/typeDefs';
+import resolvers from './graphql/resolvers';
+
+startServer({ typeDefs, resolvers });
